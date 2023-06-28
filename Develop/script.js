48e8fb43cd3c9a06a225f1102a7707c64097ad4e
Julie Kim <prettytweety2000@gmail.com>
2023-06-28 21:28:20 +1000

    day js linked

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
+
+//$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,15 +12,31 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  var saveBtn = document.querySelectorAll('#btn');
+  saveBtn.addEventListener('click', )
+  //DOM traversal document.location. aosdifjads
+
+  //local storage.....
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  // if (time > now) { class = future} 
+  // else if (time = now) {class = present}
+  // else {class = past}
+
+  var 
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+//get value 
+
+
+
+// TODO: Add code to display the current date in the header of the page.
+var today = dayjs();
+$('#currentDay').text(today.format('dddd, MMMM DD'));
